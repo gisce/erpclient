d598217591ac031026787d5eccfecb2606843dff
Eduard Carreras <ecarreras@gmail.com>
2018-11-06 04:41:49 -0800

    Bump to v5.1.1-pre

--- bin/release.py
+++ bin/release.py
@@ -20,7 +20,7 @@
 #
 
 name = 'openerp-client'
-version = '5.1.0'
+version = '5.1.1-pre'
 description = 'OpenERP Client'
 long_desc = '''OpenERP is a complete ERP and CRM. The main features are accounting (analytic
 and financial), stock management, sales and purchases management, tasks
