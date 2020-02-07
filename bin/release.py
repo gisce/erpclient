b9f4fe256f8da27b5d45694448eef39f48087269
Eduard Carreras <ecarreras@gisce.net>
2020-02-07 09:46:23 +0100

    Bump to v5.2.3

--- bin/release.py
+++ bin/release.py
@@ -20,7 +20,7 @@
 #
 
 name = 'openerp-client'
-version = '5.2.3-rc3'
+version = '5.2.3'
 description = 'OpenERP Client'
 long_desc = '''OpenERP is a complete ERP and CRM. The main features are accounting (analytic
 and financial), stock management, sales and purchases management, tasks
