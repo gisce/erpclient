bb4c7fe1f76e90c3a0bbca36adcd5e7681053e16
susu105 <davidsuarez92@gmail.com>
2022-03-30 13:21:34 +0200

    bump to v6.0.0

--- bin/release.py
+++ bin/release.py
@@ -20,7 +20,7 @@
 #
 
 name = 'openerp-client'
-version = '5.4.0.dev'
+version = '6.0.0'
 description = 'OpenERP Client'
 long_desc = '''OpenERP is a complete ERP and CRM. The main features are accounting (analytic
 and financial), stock management, sales and purchases management, tasks
